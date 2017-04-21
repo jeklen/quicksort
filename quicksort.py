6986e18df2d93eb1d5b4003e5f37fbba0fe41d25
Jek <zhql0907@outlook.com>
2017-04-21 16:16:39 +0800

    quicksort in python

---
+++ quicksort.py
@@ -0,0 +1,22 @@
+def quicksort(A, p, r):
+    if (p < r):
+        q = partition(A, p, r)
+        quicksort(A, p, q-1)
+        quicksort(A, q+1, r)
+
+# when partitioning, data is three parts: smaller, bigger, unknown
+def partition(A, p, r):
+    x = A[r]
+    i = p - 1
+    # notice that j is from p to r - 1
+    for j in range(p, r):
+        if (A[j] <= x):
+            i = i + 1
+
+            A[i], A[j] = A[j], A[i]
+    A[r], A[i+1] = A[i+1], A[r]
+    return i + 1
+
+A = [1, 9, 2, 3, -1, 10]
+quicksort(A, 0, 5)
+print(A)
